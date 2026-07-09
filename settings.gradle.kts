2aecee302dfd8cf71957bb5cbcfbb86d1b03fd1d
MANAV <267060647+manav-bidawat@users.noreply.github.com>
2026-07-09 06:36:51 +0530

    Add JogAmp repository for Linux packaging

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -11,6 +11,7 @@ dependencyResolutionManagement {
     repositories {
         google()
         mavenCentral()
+        maven { url = uri("https://jogamp.org/deployment/maven") }
         maven { url = uri("https://jitpack.io") }
     }
 }
